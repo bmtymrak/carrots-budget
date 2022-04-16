73d8dc43a67407219c18789a93bbe33d279a1199
Brennan <bmtymrak@gmail.com>
2022-04-15 20:32:14 -0400

    Update authentication views and navigation for logged in or loggout user

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(
+                self, request.method.lower(), self.http_method_not_allowed
+            )
+        else:
+            handler = self.http_method_not_allowed
+
+        if self.request.user:
+            return redirect("yearly_list")
+        else:
+            return handler(request, *args, **kwargs)
