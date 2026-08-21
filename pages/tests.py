42e446241398e7dc78d9613f20456f372c5aabda
Brennan <bmtymrak@gmail.com>
2026-08-21 14:57:08 -0400

    Remove unnecessary tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,4 @@
-import re
-
-from django.conf import settings
+from django.template.loader import render_to_string
 from django.test import SimpleTestCase, TestCase
 from django.urls import reverse
 
@@ -12,55 +10,29 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
 
-class FrontendTemplateMarkupTests(SimpleTestCase):
-    def template_sources(self):
-        template_root = settings.BASE_DIR / "templates"
-        for template_path in sorted(template_root.rglob("*.html")):
-            yield template_path.relative_to(template_root), template_path.read_text()
+class SharedFrontendComponentTests(SimpleTestCase):
+    def test_icon_action_renders_semantic_button_and_link_variants(self):
+        context = {
+            "action_label": "Edit purchase",
+            "action_url": "/purchases/1/edit/",
+            "icon_path": "images/edit-pencil.svg",
+        }
 
-    def test_templates_avoid_known_invalid_action_markup(self):
-        for template_name, source in self.template_sources():
-            with self.subTest(template=template_name):
-                self.assertNotIn("href=''", source)
-                self.assertNotIn('href=""', source)
-                for line in source.splitlines():
-                    if "<button" in line:
-                        self.assertNotIn("</a>", line)
+        button_markup = render_to_string("_includes/icon_action.html", context)
+        link_markup = render_to_string(
+            "_includes/icon_action.html", {**context, "element": "link"}
+        )
 
-    def test_templates_do_not_use_empty_class_attributes(self):
-        for template_name, source in self.template_sources():
-            with self.subTest(template=template_name):
-                self.assertNotRegex(source, r"\bclass=(['\"])\s*\1")
+        self.assertIn('<button class="button-blank" type="button"', button_markup)
+        self.assertIn('aria-label="Edit purchase"', button_markup)
+        self.assertIn('<a class="button-blank"', link_markup)
+        self.assertIn('aria-label="Edit purchase"', link_markup)
+        self.assertIn('href="/purchases/1/edit/"', link_markup)
 
-    def test_template_images_define_alternative_text(self):
-        for template_name, source in self.template_sources():
-            with self.subTest(template=template_name):
-                for image in re.findall(r"<img\b[^>]*>", source):
-                    self.assertRegex(image, r"\balt=(['\"]).*?\1")
-                    self.assertNotRegex(image, r'\bwidth="\d+px"')
+    def test_modal_shell_renders_native_dialog_and_close_control(self):
+        modal_markup = render_to_string("_includes/modal_shell.html")
 
-    def test_icon_action_controls_are_explicitly_labelled(self):
-        for template_name, source in self.template_sources():
-            with self.subTest(template=template_name):
-                for line in source.splitlines():
-                    if 'class="button-blank"' in line:
-                        self.assertIn("aria-label=", line)
-                        if "<button" in line:
-                            self.assertIn('type="button"', line)
-                        if "<a" in line:
-                            self.assertIn("href=", line)
-                    if 'class="list-nav-icon"' in line:
-                        self.assertIn('alt=""', line)
-                        self.assertIn('aria-hidden="true"', line)
-
-    def test_modal_shell_has_dialog_and_close_semantics(self):
-        modal_shell = (
-            settings.BASE_DIR / "templates/_includes/modal_shell.html"
-        ).read_text()
-
-        self.assertIn('<dialog id="modal"', modal_shell)
-        self.assertIn('aria-label="Dialog"', modal_shell)
-        self.assertIn('closedby="any"', modal_shell)
-        self.assertNotIn('class="overlay', modal_shell)
-        self.assertIn('method="dialog"', modal_shell)
-        self.assertIn('aria-label="Close dialog"', modal_shell)
+        self.assertIn('<dialog id="modal"', modal_markup)
+        self.assertIn('closedby="any"', modal_markup)
+        self.assertIn('method="dialog"', modal_markup)
+        self.assertIn('aria-label="Close dialog"', modal_markup)
